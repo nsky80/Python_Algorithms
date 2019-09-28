52819e937c5f7cd40a9b35735ed937a763cda98b
Satish NSKY80 <satishkumary80@gmail.com>
2019-09-29 03:42:03 +0530

    Codechef September Lunchtime

--- ds_python/py_linked_list/concurrent2.py
+++ ds_python/py_linked_list/concurrent2.py
@@ -3,22 +3,20 @@ class Node:
         self.__data = data
         self.__next = None
 
+    def set_data(self, data):
+        self.__data = data
+
     def get_data(self):
         return self.__data
 
-    def set_data(self,data):
-        self.__data = data
+    def set_next(self, next_node):
+        self.__next = next_node
 
     def get_next(self):
         return self.__next
 
-    def set_next(self,next_node):
-        self.__next = next_node
-
 
 class LinkedList:
-    cnt = 1
-
     def __init__(self):
         self.__head = None
         self.__tail = None
@@ -29,26 +27,27 @@ class LinkedList:
     def get_tail(self):
         return self.__tail
 
-    def add(self, data):
-        node = Node(data)
-
-        if not self.get_head():
-            self.__head = node
-        else:
-            self.__tail.set_next(node)
-        self.__tail = node
-
     def display(self):
-        temp = self.__head
+        temp = self.get_head()
         while temp:
             print(temp.get_data())
             temp = temp.get_next()
 
+    def add(self, data):
+        new_node = Node(data)
+        if not self.get_head():
+            self.__head = new_node
+            self.__tail = new_node
+        else:
+            tail = self.get_tail()
+            tail.set_next(new_node)
+            self.__tail = new_node
 
-my_list = LinkedList()
-my_list.add(50)
-my_list.add(20)
-my_list.display()
-my_list.add(30)
-my_list.display()
-# print(item_node.get_data())
+
+if __name__ == "__main__":
+    List = LinkedList()
+    List.add("Sugar")
+    List.add("Vegetables")
+    # item_node = Node("Sugar")
+
+    List.display()
